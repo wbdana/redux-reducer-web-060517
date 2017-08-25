59ed139c55deb136792992e91bc3c8c9ccf8f4b9
wbdana <william.b.dana@gmail.com>
2017-08-25 13:33:42 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,13 @@
 export function manageFriends(state, action){
+  switch (action.type) {
+    case 'ADD_FRIEND':
+      return Object.assign({}, {friends: [...state.friends, action.friend]})
+    case 'REMOVE_FRIEND':
+      const indexToRemove = state.friends.findIndex( (friend) => friend.id === action.id )
+      const firstBunch = Object.assign([], state.friends.slice(0, indexToRemove))
+      const secondBunch = Object.assign(state.friends.slice(indexToRemove + 1))
+      return Object.assign({friends: firstBunch.concat(secondBunch)})
+    default:
+      return state
+  }
 }
